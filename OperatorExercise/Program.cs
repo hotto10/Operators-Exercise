43ccbdd6e58a9f576edc5d61316308964d8ca8c2
heather otto <heatherotto32@yahoo.com>
2022-10-18 14:42:06 -0500

    finished exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace OperatorExercise
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            int quotient = (a / b);
+            int remainder = (a % b);
+            if (a == 17 && b == 4)
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            else
+                Console.WriteLine("false");
+
+            // Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var results = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {results}");
+            
         }
+
+        static double AreaOfCircle(double radius)
+        {
+            
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
     }
 }
